ba0ca7dc9197ca317bb16a13cbd167946ae2530d
jihyeongeun <hyeongeun0924@gmail.com>
2024-04-12 10:07:42 +0900

    test

--- src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
+++ src/test/java/com/ohgiraffers/bootproject/service/CalculatorServiceTests.java
@@ -24,7 +24,8 @@ class CalculatorServiceTests {
 	private static Stream<Arguments> provideDTOSource(){
 		return Stream.of(
 			Arguments.of(new CalculatorDTO(1, 2)),
-			Arguments.of(new CalculatorDTO(3, 6))
+			Arguments.of(new CalculatorDTO(3, 6)),
+			Arguments.of(new CalculatorDTO(3, 7))
 
 		);
 	}
